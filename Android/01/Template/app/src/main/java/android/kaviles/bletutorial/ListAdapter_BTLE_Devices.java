123183abb700782b9018803fcd884d01ff1f1cf5
Kelvin Aviles <kaviles@vt.edu>
2016-05-09 14:13:45 -0400

    Added template code for BLE Tutorial 01.

---
+++ Android/01/Template/app/src/main/java/android/kaviles/bletutorial/ListAdapter_BTLE_Devices.java
@@ -0,0 +1,41 @@
+package android.kaviles.bletutorial;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Kelvin on 5/7/16.
+ */
+public class ListAdapter_BTLE_Devices extends ArrayAdapter<BTLE_Device> {
+
+    Activity activity;
+    int layoutResourceID;
+    ArrayList<BTLE_Device> devices;
+
+    public ListAdapter_BTLE_Devices(Activity activity, int resource, ArrayList<BTLE_Device> objects) {
+        super(activity.getApplicationContext(), resource, objects);
+
+        this.activity = activity;
+        layoutResourceID = resource;
+        devices = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            LayoutInflater inflater =
+                    (LayoutInflater) activity.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(layoutResourceID, parent, false);
+        }
+
+        return convertView;
+    }
+}
